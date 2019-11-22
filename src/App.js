29c07b5134866dbabb8e25e938fa512aee59682d
pengxu <pengxu19890115@gmail.com>
2019-11-21 19:17:56 -0800

    refs function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import './App.css';
+import PropTypes from 'prop-types';
 
-function App() {
+const App = ({ onNewColor = f => f }) => {
+  let title, color;
+  const submit = e => {
+    e.preventDefault();
+    onNewColor(title.value, color.value);
+    title.value = '';
+    color.value = '#00000';
+    title.focus();
+  };
+
   return (
-    <div className="App">
-      <p>
-        Edit <code>src/App.js</code> and save to reload.
-      </p>
-    </div>
+    <form onSubmit={submit}>
+      <input
+        ref={input => (title = input)}
+        type="text"
+        placeholder="color title..."
+        required
+      />
+      <input ref={input => (color = input)} type="color" />
+      <button>Add</button>
+    </form>
   );
-}
+};
+
+App.propTypes = {
+  onNewColor: PropTypes.func
+};
 
 export default App;
